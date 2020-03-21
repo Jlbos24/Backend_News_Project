61b8c3dbc0603c35e19734291b40a7246cbcd9ae
JlBos24 <jlbosman24@gmail.com>
2020-03-21 17:51:08 +0000

    endpoints update

--- router/apiRouter.js
+++ router/apiRouter.js
@@ -10,12 +10,7 @@ apiRouter.use("/articles", articlesRouter);
 apiRouter.use("/comments", commentsRouter);
 
 apiRouter.get("/", (req, res, next) => {
-  // const pathsArray = [];
-  // apiRouter.stack.forEach(stack => {
-  //   pathsArray.push(stack);
-  // });
-  // //console.log(apiRouter.stack);
-  // console.log(pathsArray[0].regexp, pathsArray[0].keys);
+  res.status(200).sendFile("endpoints.json");
 });
 
 module.exports = apiRouter;
